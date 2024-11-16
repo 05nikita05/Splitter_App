49d4585603ac36c4a31e66e7db41fe97b81e3e65
Nikita dwivedi <nikitadwivedi205@gmail.com>
2024-11-16 22:15:28 +0530

    upadated axios link

--- splitterApp/axiosInstance.jsx
+++ splitterApp/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token'); 
 
 const axiosInstance = axios.create({
-  baseURL: 'https://splitter-app-6fyv.vercel.app/',
+  baseURL: 'https://splitter-app.onrender.com/',
 
 });
 // Add interceptor to include the token
