1bc4c86f722331bbca4b6a69b8eb9e2f92ed2440
Nikita dwivedi <nikitadwivedi205@gmail.com>
2024-11-16 22:19:48 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS setup for allowing frontend access
 app.use(cors({
-  origin: 'https://splitter-app.onrender.com', // Frontend URL (update as needed)
+  origin: 'https://splitter-app-6fyv.vercel.app', // Frontend URL (update as needed)
   methods: ['GET', 'POST', 'PUT','DELETE'], // Allow the appropriate HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
