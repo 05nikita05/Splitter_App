da4a8be343f50265bd33fee039e34b626e58f212
Nikita dwivedi <nikitadwivedi205@gmail.com>
2024-11-20 15:39:09 +0530

    updated backend cors origin

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS setup for allowing frontend access
 app.use(cors({
-  origin: ['https://splitter-app-wheat.vercel.app/','http://localhost:5173'], // Frontend URL (update as needed)
+  origin: [process.env.FRONTEND_URL,'http://localhost:5173'], // Frontend URL (update as needed)
   methods: ['GET', 'POST', 'PUT','DELETE'], // Allow the appropriate HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
