971b4fc0181b086b07567b6279423e7d964fda3a
Nikita dwivedi <nikitadwivedi205@gmail.com>
2024-11-20 15:50:29 +0530

    update viteConfig

--- splitterApp/vite.config.js
+++ splitterApp/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // This should match your output directory
+  },
+  base: '/', // Ensures proper path resolution for Vercel deployment
 })
